825ef5d6938712660059b707404c70e39256df28
Ruslan Sadykov <interligo@yandex.ru>
2021-04-01 17:58:25 +0300

    Remove code block

--- binary_search_with_tests.py
+++ binary_search_with_tests.py
@@ -35,31 +35,31 @@ class BinarySearch:
         return None
 
 
-bs = BinarySearch()
-simple_list = [2, 3, 4, 13, 40]
-
-
-class TestBinarySearch(unittest.TestCase):
-  def setUp(self):
-    print ("... %s" % self._testMethodName)
+class BinarySearchTest(unittest.TestCase):
+  def setUp(self) -> None:
+    print(f'Run {self._testMethodName}.')
   
-  def test_iterative_binary_search_with_simple_list(self):    
+  def test_iterative_binary_search_with_simple_list(self) -> None:    
     item, expected_index = 3, 1
     index = bs.search_iterative(simple_list, item)
     self.assertEqual(expected_index, index)
 
-  def test_recoursive_binary_search_with_simple_list(self):
+  def test_recoursive_binary_search_with_simple_list(self) -> None:
     item, expected_index = 3, 1    
     low, high = 0, len(simple_list) - 1
     index = bs.search_recursive(simple_list, low, high, item)
     self.assertEqual(expected_index, index)
 
-  def test_search_for_nonexistent_item(self):    
+  def test_search_for_nonexistent_item(self) -> None:    
     item, expected_result = 100, None
     index = bs.search_iterative(simple_list, item)
     self.assertEqual(expected_result, index)
+
     
+bs = BinarySearch()
+simple_list = [2, 3, 4, 13, 40]
     
+  
 if __name__ == '__main__':
     unittest.main()
     
